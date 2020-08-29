81d469ad8f41b46fcc45c508da0a1eeb071b39fc
Oliad-R <runol@utschools.ca>
2020-08-28 23:09:32 -0400

    Built the basic layout + adding tasks functionality

---
+++ src/app/src/main/java/com/oliadr/todoapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.oliadr.todoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<String> ToDoList;
+    ArrayAdapter<String> arrayAdapter;
+    ListView listView;
+    EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ToDoList = new ArrayList<>();
+        arrayAdapter = new ArrayAdapter<>(this,R.layout.list_view_layout,ToDoList);
+        listView = findViewById(R.id.list_view);
+
+        listView.setAdapter(arrayAdapter);
+
+        editText = findViewById(R.id.id_edit_text);
+    }
+    public void addItemToList(View view) {
+        ToDoList.add(editText.getText().toString());
+        arrayAdapter.notifyDataSetChanged();
+        editText.setText("");
+    }
+}
